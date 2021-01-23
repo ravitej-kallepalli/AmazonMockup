a816097f48fca072d84056d652002ae68af9a42a
ntippa <nalini.banarasi@gmail.com>
2021-01-22 22:20:19 -0500

    Initial commit-LaunchActivity

---
+++ app/src/main/java/com/ravitej/amazonmockup/LaunchActivity.kt
@@ -0,0 +1,10 @@
+package com.ravitej.amazonmockup
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
